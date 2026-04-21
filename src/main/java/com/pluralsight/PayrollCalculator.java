08028b2564de4333230e590b0321c1a71938b46e
Tushar Islam <tushar060703@gmail.com>
2026-04-21 09:41:15 -0400

    Updated with BufWriter

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,16 +1,22 @@
 package com.pluralsight;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.text.ParseException;
+import java.util.Scanner;
 
 public class PayrollCalculator
 {
     static void main() throws IOException {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the name of the file employee file to process: ");
+        BufferedReader bufReader = new BufferedReader(new FileReader(scanner.nextLine()));
+        System.out.print("Enter the name of the payroll file to create: ");
+        BufferedWriter bufWriter = new BufferedWriter(new FileWriter(scanner.nextLine()));
+
+        String text;
         String input;
-        BufferedReader bufReader = new BufferedReader(new FileReader("employees.csv"));
+
         int count = 0;
 
         bufReader.readLine();
@@ -19,7 +25,9 @@ public class PayrollCalculator
             String[] data = input.split("\\|");
             Employee employee = new Employee(Integer.parseInt(data[0]), data[1], Double.parseDouble(data[2]), Double.parseDouble(data[3]));
             System.out.println(employee.getEmployeeID() + " " +  employee.getName() + " " + employee.getGrossPay());
+            bufWriter.write(employee.getEmployeeID() + " | " +  employee.getName() + " | " + employee.getGrossPay() + "\n");
         }
+        bufWriter.close();
 
     }
 }
